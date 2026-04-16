2b8ed1979669d72aa45f55654c56955559418581
Overn-clo <thoma.fmartin@gmail.com>
2026-04-16 14:52:00 +0200

    feat: squelette header

---
+++ assets/include/header.php
@@ -0,0 +1,21 @@
+<link rel="stylesheet" href="./css/header.css ?v=<?= time() ?>">
+<header>
+    <div class="nav-gauche">
+        <div class="logo">
+            <a href="index.php"><img src="./images/logo.png" alt="Logo"></a>
+            <a href="index.php" class="logo-text">Titre site</a>
+        </div>
+    </div>
+
+    <div class="nav-droite">
+        <a href="/pages/methode.php">Methode</a>
+        <a href="/pages/parcours.php">Parcours</a>
+        <a href="/pages/rdv.php">Prendre rendez-vous</a>
+    </div>
+
+
+
+
+
+
+</header>
