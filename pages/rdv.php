583f313b8d3576fa2c15da6ec8d782d34174d7a6
Overn-clo <thoma.fmartin@gmail.com>
2026-04-22 11:40:21 +0200

    feat: prendre rendez-vous

--- pages/rdv.php
+++ pages/rdv.php
@@ -7,11 +7,28 @@
     <title>Rendez-vous</title>
     <link rel="stylesheet" href="../assets/css/main.css ?v=<?= time() ?>">
     <link rel="stylesheet" href="../assets/css/rdv.css ?v=<?= time() ?>">
-    
+    <link href="https://assets.calendly.com/assets/external/widget.css" rel="stylesheet">
 </head>
 
 <body>
     <?php require_once('../assets/include/header.php'); ?>
+
+    <main>
+
+        <div class="texte_rdv">
+            <h2>Prendre rendez-vous</h2>
+            <p>Choisis un créneau directement dans mon agenda :</p>
+        </div>
+
+        <div class="calendly-wrapper">
+            <div class="calendly-inline-widget"
+                data-url="https://calendly.com/eric8124/30min?hide_event_type_details=1&hide_gdpr_banner=1"
+                style="min-width:320px; height:700px;">
+            </div>
+        </div>
+        
+        <script type="text/javascript" src="https://assets.calendly.com/assets/external/widget.js" async></script>
+    </main>
 </body>
 
 </html>
