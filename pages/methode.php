210742b0391e05dea14d68425957a64afbb328e1
Overn-clo <thoma.fmartin@gmail.com>
2026-04-23 12:38:33 +0200

    docs: maj du texte

--- pages/methode.php
+++ pages/methode.php
@@ -7,30 +7,54 @@
     <title>Méthode</title>
     <link rel="stylesheet" href="../assets/css/main.css ?v=<?= time() ?>">
     <link rel="stylesheet" href="../assets/css/methode.css ?v=<?= time() ?>">
-    
+
 </head>
 
 <body>
     <?php require_once('../assets/include/header.php'); ?>
     <main>
-        <div class="offres">
-            <div class="offre_etudiant">
-                <h1>Etudiant</h1>
-                <p>Blallaalalla</p>
-                <a class="prendre_rdv" href="rdv.php">Prendre rendez-vous</a>
-            </div>
-
-            <div class="offre_sport">
-                <h1>Sports</h1>
-                <p>Blallaalalla</p>
-                <a class="prendre_rdv" href="rdv.php">Prendre rendez-vous</a>
-            </div>
-        </div>
 
         <div class="fonctionne">
-            <h1>Comment je fonctionne</h1>
-            <p>Blallaalalla</p>
+            <div class="texte">
+                <h1>Comment je fonctionne</h1>
+                <p>Mon approche repose sur 4 piliers :<br><br>
+
+                    1. Identité & valeurs<br>
+                    Mieux se connaître pour mieux performer.<br><br>
+
+                    2. Gestion des émotions & respiration<br>
+                    Apprendre à se réguler dans l’action.<br><br>
+
+                    3. Concentration & attention<br>
+                    Être pleinement présent dans les moments clés.<br><br>
+
+                    4. Plaisir & engagement<br>
+                    Retrouver du plaisir pour libérer la performance.
+                </p>
+            </div>
         </div>
+
+        <div class="offres">
+            <div class="offre_etudiant">
+                <div class="texte">
+                    <h1>Etudiant</h1>
+                    <p>Blallaalalla</p>
+                    <a class="prendre_rdv" href="rdv.php">Prendre rendez-vous</a>
+                </div>
+            </div>
+
+            <div class="separateur"></div>
+
+            <div class="offre_sport">
+                <div class="texte">
+                    <h1>Sports</h1>
+                    <p>Blallaalalla</p>
+                    <a class="prendre_rdv" href="rdv.php">Prendre rendez-vous</a>
+                </div>
+            </div>
+        </div>
+
+        
     </main>
 </body>
 
--- pages/parcours.php
+++ pages/parcours.php
@@ -16,7 +16,49 @@
         <div class="presentation_eric">
             <div class="texte">
                 <h1>Mon Parcours</h1>
-                <p>Je suis Eric </p>
+                <div class="textes_colonnes">
+                    <div class="parcour1">
+                        <p>À 26 ans, tout s’arrête.
+
+                            Un accident sur un terrain de rugby met fin brutalement à ma pratique.
+                            Trois opérations du dos, des séquelles à la jambe… et la décision de quitter la police.
+
+                            Du jour au lendemain, je perds mes repères.
+                            Le terrain, l’engagement, le cadre.
+
+                            Commence alors une période de doute, de remise en question et de reconstruction.
+                            Une période difficile, mais fondatrice.
+
+                            Je fais alors un choix : me reconstruire.<br> <br> <br>
+
+                            Je reprends mes études à 26 ans et obtiens un Master 2 en école de commerce.
+                            Cette étape forge ma capacité à rebondir, à m’adapter et à avancer malgré l’incertitude.
+
+                            Je débute ensuite chez Airbus en tant que consultant en transfert de connaissances, avant de rejoindre les écoles EPSI pour développer le campus de Toulouse.
+
+                            Depuis plus de 6 ans, j’accompagne des étudiants dans des moments clés : orientation, recherche d’alternance, confiance en soi, passage à l’action.
+                            Aujourd’hui, en tant que coordinateur national, je travaille également sur la performance des équipes.
+                        </p>
+                    </div>
+
+                    <div class="separateur"></div>
+
+                    <div class="parcours2">
+                        <p>
+                            En parallèle, je reste profondément ancré dans le rugby.
+                            Diplômé entraîneur, j’ai accompagné pendant 5 ans des équipes cadets et juniors au niveau national.
+
+                            C’est là qu’une évidence s’impose :
+                            la performance se joue avant tout dans la tête.
+
+                            Je me forme alors à la préparation mentale, notamment avec Olivier Lepretre et Christian Ramos.
+
+                            Je complète cette approche avec la Catch & Think Academy, spécialisée dans la concentration et l’attention.
+
+                            Aujourd’hui, j’accompagne les jeunes rugbymen et les étudiants à performer dans des environnements exigeants.
+                        </p>
+                    </div>
+                </div>
             </div>
 
             <div class="img_Eric">
@@ -27,7 +69,11 @@
         <div class="philosophie">
             <div class="texte">
                 <h1>Ma philosophie</h1>
-                <p>Po</p>
+                <p>Pression, doute, perte de plaisir, baisse de performance.<br><br>
+                    Mon rôle :
+                    remettre de la clarté, de la confiance, de la concentration et du plaisir.<br><br>
+                    La performance doit être vécue pleinement.
+                </p>
             </div>
         </div>
 
